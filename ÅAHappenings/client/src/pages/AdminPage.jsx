c0e37960970dff2018fa051006516334f31d180a
AntonNasman <104820773+AntonNasman@users.noreply.github.com>
2025-03-05 22:39:29 +0200

    admin page functionality, no scrollbar eventlist, blur dialog background

--- ÅAHappenings/client/src/pages/AdminPage.jsx
+++ ÅAHappenings/client/src/pages/AdminPage.jsx
@@ -1,9 +1,14 @@
 import React, { useState, useEffect } from "react";
 import "../styles/background.css";
 import "../styles/AdminPage.css";
+import { useEventsContext } from "../hooks/useEventsContext";
+import { useNavigate } from "react-router-dom";
+import { useAuthContext } from "../hooks/useAuthContext";
 
 const AdminPage = () => {
-  const [events, setEvents] = useState([]);
+  const {user} = useAuthContext();
+  const navigate = useNavigate();
+  const {events, dispatch} = useEventsContext();
   const [users, setUsers] = useState([]);
   const [whitelist, setWhitelist] = useState([]); // Whitelisted emails
   const [emailInput, setEmailInput] = useState("");
@@ -13,53 +18,159 @@ const AdminPage = () => {
   const [eventsSearch, setEventsSearch] = useState("");
   const [whitelistSearch, setWhitelistSearch] = useState("");
 
+  // useStates for filtered stuff
+  const [filteredEvents, setFilteredEvents] = useState([]);
+  const [filteredUsers, setFilteredUsers] = useState([]);
+  const [filteredWhitelists, setFilteredWhitelists] = useState([]);
+
+
+  //fetch events, users and whitelists from db
   useEffect(() => {
-    // Fetch Events from MongoDB
-    fetch("http://localhost:5050/events/")
-      .then((response) => response.json())
-      .then((data) => setEvents(data))
-      .catch((error) => console.error("Error fetching events:", error));
 
-    // Fetch Users from MongoDB
-    fetch("http://localhost:5050/users/")
-      .then((response) => response.json())
-      .then((data) => setUsers(data))
-      .catch((error) => console.error("Error fetching users:", error));
-  }, []);
+    //event fetch
+    const fetchEvents = async () =>  {
+      const response = await fetch("http://localhost:5050/event");
+      console.log(response)
+      const json = await response.json();
 
-  const handleEventClick = (eventId) => {
-    console.log("Clicked event:", eventId);
-  };
+      if (!response.ok) {
+        const message = `An error occurred: ${response.statusText}`;
+        console.error(message);
+        return;
+      }
+      dispatch({type: "SET_EVENTS", payload: json})
+      setFilteredEvents(json);
+    }
 
-  const handleUserClick = (userId) => {
-    console.log("Clicked user:", userId);
-  };
+    //user fetch
+    const fetchUsers = async () =>  {
+      const response = await fetch("http://localhost:5050/organizer", {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          "Authorization": `Bearer ${user.token}`
+        }
+      });
+      console.log(response)
+      const json = await response.json();
+
+      if (!response.ok) {
+        const message = `An error occurred: ${response.statusText}`;
+        console.error(message);
+        return;
+      }
+      setFilteredUsers(json);
+      setUsers(json);
+    }
+
+    //whitelist fetch
+    const fetchWhitelist = async () =>  {
+      const response = await fetch("http://localhost:5050/whitelist", {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          "Authorization": `Bearer ${user.token}`
+        }
+      });
+      console.log(response)
+      const json = await response.json();
+
+      if (!response.ok) {
+        const message = `An error occurred: ${response.statusText}`;
+        console.error(message);
+        return;
+      }
+      setFilteredWhitelists(json);
+      setWhitelist(json);
+    }
+
+    fetchEvents();
+    fetchUsers();
+    fetchWhitelist();
+    return;
+  }, [dispatch]);
+
+  //userfiltering
+  useEffect(() => {
+    const filtered = userSearchFilter(users);
+    setFilteredUsers(filtered);
+  }, [usersSearch]);
+
+  const userSearchFilter = (usersToFilter) => {
+    if(usersSearch.length != 0) {
+      return usersToFilter.filter((user) => (user.username).toLocaleLowerCase().includes(usersSearch.toLocaleLowerCase()));
+    }
+    return usersToFilter;
+  }
+
+  //eventfiltering
+  useEffect(() => {
+    const filtered = eventSearchFilter(events);
+    setFilteredEvents(filtered);
+  }, [eventsSearch]);
+
+  const eventSearchFilter = (eventsToFilter) => {
+    if(eventsSearch.length != 0) {
+      return eventsToFilter.filter((event) => (event.title).toLocaleLowerCase().includes(eventsSearch.toLocaleLowerCase()));
+    }
+    return eventsToFilter;
+  }
+
+  //whitelist filtering
+  useEffect(() => {
+    const filtered = whitelistSearchFilter(whitelist)
+    setFilteredWhitelists(filtered);
+  }, [whitelistSearch]);
+
+  const whitelistSearchFilter = (whitelistsToFilter) => {
+    if(whitelistSearch.length != 0) {
+      return whitelistsToFilter.filter((wl) => (wl.email).toLocaleLowerCase().includes(whitelistSearch.toLocaleLowerCase()));
+    }
+    return whitelistsToFilter;
+  }
+
 
   // Add Email to Whitelist
-  const addToWhitelist = () => {
+  const addToWhitelist = async () => {
     if (emailInput.trim() && !whitelist.includes(emailInput)) {
-      setWhitelist([...whitelist, emailInput]);
+      const email = emailInput;
+      const response = await fetch("http://localhost:5050/whitelist/add", {
+        method: "POST",
+        body: JSON.stringify({email}),
+        headers: {
+          "Content-Type": "application/json",
+          "Authorization": `Bearer ${user.token}`
+        },
+      });
+      console.log(response);
+  
+      if (!response.ok) {
+        const message = `An error occurred: ${response.statusText}`;
+        console.error(message);
+      }
       setEmailInput(""); // Clear input field
     }
   };
 
   // Remove Email from Whitelist
-  const removeFromWhitelist = (email) => {
-    setWhitelist(whitelist.filter((item) => item !== email));
+  const removeFromWhitelist = async (email) => {
+    const response = await fetch("http://localhost:5050/whitelist/remove", {
+      method: "DELETE",
+      body: JSON.stringify({email}),
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": `Bearer ${user.token}`
+      },
+    });
+    console.log(response);
+
+    if (!response.ok) {
+      const message = `An error occurred: ${response.statusText}`;
+      console.error(message);
+      return;
+    }
   };
 
-  // Filter lists based on search queries
-  const filteredUsers = users.filter((user) =>
-    user.username.toLowerCase().includes(usersSearch.toLowerCase())
-  );
-
-  const filteredEvents = events.filter((event) =>
-    event.name.toLowerCase().includes(eventsSearch.toLowerCase())
-  );
-
-  const filteredWhitelist = whitelist.filter((email) =>
-    email.toLowerCase().includes(whitelistSearch.toLowerCase())
-  );
 
   return (
     <div>
@@ -78,16 +189,17 @@ const AdminPage = () => {
             className="search-bar"
           />
           <ul>
-            {filteredUsers.map((user) => (
+            {filteredUsers && filteredUsers.map((user) => (
               <li
                 key={user._id}
-                onClick={() => handleUserClick(user._id)}
+                onClick={() => navigate(`/profile/${user._id}`)}
                 className="clickable-item"
               >
                 {user.username}
               </li>
             ))}
           </ul>
+
         </div>
 
         {/* Events Column */}
@@ -101,13 +213,13 @@ const AdminPage = () => {
             className="search-bar"
           />
           <ul>
-            {filteredEvents.map((event) => (
+            {filteredEvents && filteredEvents.map((event) => (
               <li
                 key={event._id}
-                onClick={() => handleEventClick(event._id)}
+                onClick={() => navigate(`/event/${event._id}`)}
                 className="clickable-item"
               >
-                {event.name}
+                {event.title}
               </li>
             ))}
           </ul>
@@ -132,13 +244,13 @@ const AdminPage = () => {
             onChange={(e) => setWhitelistSearch(e.target.value)}
             className="search-bar"
           />
-          <ul className="whitelist-list">
-            {filteredWhitelist.map((email, index) => (
-              <li key={index} className="whitelist-item">
-                {email}
+          <ul >
+            {filteredWhitelists && filteredWhitelists.map((emailIdPair) => (
+              <li key={emailIdPair._id} className="whitelist-item">
+                {emailIdPair.email}
                 <span
                   className="remove-btn"
-                  onClick={() => removeFromWhitelist(email)}
+                  onClick={() => removeFromWhitelist(emailIdPair.email)}
                 >
                   ✖
                 </span>
--- ÅAHappenings/server/routes/event.js
+++ ÅAHappenings/server/routes/event.js
@@ -13,7 +13,7 @@ import {requireAuth} from '../middleware/requireAuth.js'
 
 const router = express.Router()
 
-//get all workouts
+//get all events
 router.get('/', getEvents)
 
 //get all events for logged in organizer
@@ -32,97 +32,3 @@ router.delete('/:id', requireAuth ,deleteEvent)
 router.patch('/:id', requireAuth, updateEvent)
 
 export default router;
-
-/*
-import express from "express";
-import db from "../db/connection.js";
-import { ObjectId } from "mongodb";
-
-const router = express.Router();
-
-//get all events
-router.get("/", async (req, res) => {
-    let collection = await db.collection("events");
-    let results = await collection.find({}).toArray();
-    res.send(results).status(200);
-});
-
-//get event by id
-router.get("/:id", async (req, res) => {
-    let collection = await db.collection("events");
-    let query = { _id: new ObjectId(req.params.id) };
-    let result = await collection.findOne(query);
-
-    if (!result) res.send("Not found").status(404);
-    else res.send(result).status(200);
-});
-
-//create new event
-router.post("/", async (req, res) => {
-    try {
-      let newDocument = {
-        title: req.body.title,
-        description: req.body.description,
-        location: req.body.location,
-        date: req.body.date,
-        time: req.body.time,
-        how: req.body.how,
-        price: req.body.price,
-        link: req.body.link,
-        membersOnly: req.body.membersOnly,
-        tags: req.body.tags,
-      };
-      let collection = await db.collection("events");
-      let result = await collection.insertOne(newDocument);
-      res.send(result).status(204);
-    } catch (err) {
-      console.error(err);
-      res.status(500).send("Error adding record");
-    }
-});
-
-// This section will help you update a record by id.
-router.patch("/:id", async (req, res) => {
-  try {
-    const query = { _id: new ObjectId(req.params.id) };
-    const updates = {
-      $set: {
-        title: req.body.title,
-        description: req.body.description,
-        location: req.body.location,
-        date: req.body.date,
-        time: req.body.time,
-        how: req.body.how,
-        price: req.body.price,
-        link: req.body.link,
-        membersOnly: req.body.membersOnly,
-        tags: req.body.tags
-      },
-    };
-
-    let collection = await db.collection("events");
-    let result = await collection.updateOne(query, updates);
-    res.send(result).status(200);
-  } catch (err) {
-    console.error(err);
-    res.status(500).send("Error updating record");
-  }
-});
-
-// This section will help you delete a record
-router.delete("/:id", async (req, res) => {
-  try {
-    const query = { _id: new ObjectId(req.params.id) };
-
-    const collection = db.collection("events");
-    let result = await collection.deleteOne(query);
-
-    res.send(result).status(200);
-  } catch (err) {
-    console.error(err);
-    res.status(500).send("Error deleting record");
-  }
-});
-
-export default router;
-*/
